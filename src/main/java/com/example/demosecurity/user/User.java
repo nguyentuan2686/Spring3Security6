c865fa8c4e184889500672b7d915f9a733ef4f79
Tuan. Nguyen Anh - CMC Global DU1.22 <natuan17@cmcglobal.vn>
2023-02-23 17:25:07 +0700

    setup db postgres in docker

---
+++ src/main/java/com/example/demosecurity/user/User.java
@@ -0,0 +1,27 @@
+package com.example.demosecurity.user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    private Integer id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
