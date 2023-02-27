a48bc1baa127d0ca2db317a973b9924b39adfc0e
Tuan. Nguyen Anh - CMC Global DU1.22 <natuan17@cmcglobal.vn>
2023-02-27 17:05:44 +0700

    add jwt

---
+++ src/main/java/com/example/demosecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.demosecurity.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
--- src/main/java/com/example/demosecurity/user/User.java
+++ src/main/java/com/example/demosecurity/user/User.java
@@ -6,6 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -13,7 +19,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -23,4 +29,41 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
 }
